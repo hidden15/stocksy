e07b440ff51147bf8b573ec842f81df1daa56cd6
Naman Verma <namanverma7089@gmail.com>
2026-07-30 22:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import pandas_ta_classic as ta
 import numpy as np
 import plotly.graph_objects as go
 from sklearn.ensemble import RandomForestClassifier
